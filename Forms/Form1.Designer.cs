eeabcfe0a1ebd10f253cfcf682a76681124c2461
adnKsharp <darkmark404@protonmail.com>
2022-07-21 21:55:45 -0600

    First...

---
+++ Forms/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Forms
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Font = new System.Drawing.Font("Hack NF", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(73, 15);
+			this.button1.Margin = new System.Windows.Forms.Padding(6);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(46, 46);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.B1C);
+			// 
+			// button2
+			// 
+			this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button2.FlatAppearance.BorderSize = 0;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Font = new System.Drawing.Font("Hack NF", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(131, 15);
+			this.button2.Margin = new System.Windows.Forms.Padding(6);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(46, 46);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.B2C);
+			// 
+			// button3
+			// 
+			this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button3.FlatAppearance.BorderSize = 0;
+			this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button3.Font = new System.Drawing.Font("Hack NF", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button3.Location = new System.Drawing.Point(247, 15);
+			this.button3.Margin = new System.Windows.Forms.Padding(6);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(46, 46);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.B3C);
+			// 
+			// button4
+			// 
+			this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button4.FlatAppearance.BorderSize = 0;
+			this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button4.Font = new System.Drawing.Font("Hack NF", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button4.Location = new System.Drawing.Point(189, 15);
+			this.button4.Margin = new System.Windows.Forms.Padding(6);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(46, 46);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.B4C);
+			// 
+			// button5
+			// 
+			this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(17)))), ((int)(((byte)(23)))));
+			this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button5.FlatAppearance.BorderSize = 0;
+			this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button5.Font = new System.Drawing.Font("Hack NF", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button5.Location = new System.Drawing.Point(15, 15);
+			this.button5.Margin = new System.Windows.Forms.Padding(6);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(46, 46);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "button5";
+			this.button5.UseVisualStyleBackColor = false;
+			this.button5.Click += new System.EventHandler(this.B5C);
+			// 
+			// button7
+			// 
+			this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(17)))), ((int)(((byte)(23)))));
+			this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button7.FlatAppearance.BorderSize = 0;
+			this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button7.Font = new System.Drawing.Font("Hack NF", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button7.Location = new System.Drawing.Point(305, 15);
+			this.button7.Margin = new System.Windows.Forms.Padding(6);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(46, 46);
+			this.button7.TabIndex = 6;
+			this.button7.Text = "button7";
+			this.button7.UseVisualStyleBackColor = false;
+			this.button7.Click += new System.EventHandler(this.B7C);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(17)))), ((int)(((byte)(23)))));
+			this.ClientSize = new System.Drawing.Size(364, 76);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Font = new System.Drawing.Font("SF Pro Display", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ForeColor = System.Drawing.Color.White;
+			this.Margin = new System.Windows.Forms.Padding(6);
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.Text = "Control multimedia";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button7;
+	}
+}
+
---
+++ Forms/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Forms
+{
+	public partial class Form1 : Form
+	{
+		int[] media = { 0xC0000, 0xE0000, 0xB0000 },
+				vol = { 0x80000, 0x90000, 0xA0000 };
+
+		[DllImport("user32.dll")]
+		public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			button1.Text = "";
+			button2.Text = "";
+			button3.Text = "";
+			button4.Text = "";
+			button5.Text = "";
+			button7.Text = "";
+		}
+
+		private void B1C(object sender, EventArgs e)
+		{
+			Media(0);
+		}
+
+		private void B2C(object sender, EventArgs e)
+		{
+			Media(1);
+		}
+
+		private void B3C(object sender, EventArgs e)
+		{
+			Media(2);
+		}
+
+		private void B4C(object sender, EventArgs e)
+		{
+			Volume(0);
+		}
+
+		private void B5C(object sender, EventArgs e)
+		{
+			Volume(1);
+		}
+
+		private void B7C(object sender, EventArgs e)
+		{
+			Volume(2);
+		}
+
+		private void Media(byte k)
+		{
+			SendMessageW(Handle, 0x319, Handle, (IntPtr)media[k]);
+		}
+
+		private void Volume(byte k)
+		{
+			SendMessageW(Handle, 0x319, Handle, (IntPtr)vol[k]);
+		}
+	}
+}
